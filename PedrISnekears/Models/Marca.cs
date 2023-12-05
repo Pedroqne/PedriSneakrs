b1e1d0a678de74f650536bcadf8656c26f2b965c
Pedro Silva <pedro.conceicao2014@gmail.com>
2023-12-05 09:48:19 -0300

    first commit

---
+++ PedrISnekears/Models/Marca.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PedrISnekears.Models
+{
+	[Table("Marcas")]
+	public class Marca
+	{
+		[Key]
+		public int MarcaId { get; set; }
+		[Required(ErrorMessage = "Insira o nome da marca")]
+		[Display(Name = "Nome da marca")]
+		[StringLength(50,ErrorMessage = "O nome da marca deve ter no máximo 50 caracteres")]
+        public string MarcaNome { get; set; }
+		[Required(ErrorMessage = "Insira uma descrição para marca")]
+		[Display(Name = "Descrição da marca")]
+		[StringLength(100,MinimumLength = 30,ErrorMessage = "A descrição deve conter no minimo 30 caracteres e no máximo 100")]
+        public string Descricao { get; set; }
+
+		public List<Sneaker> Sneakers{ get; set; }
+    }
+}
---
+++ PedrISnekears/Models/Sneaker.cs
@@ -0,0 +1,17 @@
+﻿namespace PedrISnekears.Models
+{
+	public class Sneaker
+	{
+        public int SneakerId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+        public string ImgUrl { get; set; }  
+        public string ImgThumbnailUrl { get; set;}
+        public bool IsDestaque { get; set; }
+        public bool EmEstoque { get; set; }
+    }
+}
